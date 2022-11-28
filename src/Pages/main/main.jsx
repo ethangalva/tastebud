ded52c7bde4c9e1269be4e7fd838a2f9fa832a4a
ethangalva <82258388+ethangalva@users.noreply.github.com>
2022-11-28 15:07:12 -0800

    feat: +blobs ^/v | +new styling for CTA | +web responsive

--- src/Pages/main/main.jsx
+++ src/Pages/main/main.jsx
@@ -50,6 +50,8 @@ function MainView(props) {
                         </div>
                     </div>
                 </div>
+                {/* blob on top of the pageContent Section */}
+                <svg style={{position: "relative", bottom: "-3px"}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#e8dfca" fill-opacity="1" d="M0,192L26.7,192C53.3,192,107,192,160,176C213.3,160,267,128,320,122.7C373.3,117,427,139,480,154.7C533.3,171,587,181,640,160C693.3,139,747,85,800,80C853.3,75,907,117,960,122.7C1013.3,128,1067,96,1120,74.7C1173.3,53,1227,43,1280,37.3C1333.3,32,1387,32,1413,32L1440,32L1440,320L1413.3,320C1386.7,320,1333,320,1280,320C1226.7,320,1173,320,1120,320C1066.7,320,1013,320,960,320C906.7,320,853,320,800,320C746.7,320,693,320,640,320C586.7,320,533,320,480,320C426.7,320,373,320,320,320C266.7,320,213,320,160,320C106.7,320,53,320,27,320L0,320Z"></path></svg>
                 {/* container for page content */}
                 <div className={styles.pageContent}>
                     <div className={styles.pageContent_featuredTitle}>Featured Recipes</div>
@@ -80,9 +82,14 @@ function MainView(props) {
                     </div>
                     {/* call to action */}
                     <div className={styles.featuredCTA}>
-                        <a>find new recipes</a>
+                        <button className={styles.buttonCTA}>Find More Recipes</button>
                     </div>
                 </div>
+                {/* blob under of the pageContent Section */}
+                <svg style={{position: "relative", bottom: "3px"}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#e8dfca" fill-opacity="1" d="M0,192L26.7,192C53.3,192,107,192,160,176C213.3,160,267,128,320,122.7C373.3,117,427,139,480,154.7C533.3,171,587,181,640,160C693.3,139,747,85,800,80C853.3,75,907,117,960,122.7C1013.3,128,1067,96,1120,74.7C1173.3,53,1227,43,1280,37.3C1333.3,32,1387,32,1413,32L1440,32L1440,0L1413.3,0C1386.7,0,1333,0,1280,0C1226.7,0,1173,0,1120,0C1066.7,0,1013,0,960,0C906.7,0,853,0,800,0C746.7,0,693,0,640,0C586.7,0,533,0,480,0C426.7,0,373,0,320,0C266.7,0,213,0,160,0C106.7,0,53,0,27,0L0,0Z"></path></svg>
+                {/* <div className={styles.pageContentAbout}>
+                    <div className={styles.aboutTitle}>About Us</div>
+                </div> */}
             </div>
         </div>
     )
