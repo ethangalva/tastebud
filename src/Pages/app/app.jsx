fcb81633285a0ee2a903f19213fc5ba54112a8e8
ethangalva <82258388+ethangalva@users.noreply.github.com>
2022-11-29 11:30:55 -0800

    feat: +reactRouter

--- src/Pages/app/app.jsx
+++ src/Pages/app/app.jsx
@@ -6,7 +6,45 @@ import Header from '../../Components/header/header';
 import Footer from '../../Components/footer/footer';
 import Placeholder from '../../Components/placeholder/placeholder';
 
+import axios from 'axios';
+import { useState } from 'react';
+
 function App(props) {
+    // assigns default variable to search input
+    const [ input, setInput ] = useState('');
+    // declare hook for input
+    const [ inputErr, setInputErr ] = useState('');
+
+    const validate = () => {
+        let toVal = input;
+        let val = toVal.replace(/\s/g, '%20')
+        return val
+    }
+        
+    // function for submit form
+    const handleSubmit = (e) => {
+        // stop default behavior
+        e.preventDefault();
+        // defining variables 
+        // const accessPoint = 'https://api.edamam.com/api/recipes/v2';
+        let type = 'public';
+        let key = 'dfe6dff4b314211f8e59e2c208d41dde';
+        let id = 'edfd8a8e';
+        let tag = input.replace(/\s/g, '%20');
+
+        console.log(input.replace(/\s/g, '%20'))
+        // if(isVal) {
+            axios.get(
+                `https://api.edamam.com/api/recipes/v2?type=${type}&app_id=${id}&app_key=${key}&tag=${tag}`
+            ).then(response => {
+                const data = response.data;
+                console.log(data);
+            }).catch(e => {
+                console.log(e);
+            })
+        // }
+    };
+
     return(
         <div className={styles.container}>
             <div className={styles.contents}>
@@ -18,8 +56,8 @@ function App(props) {
                     <div className={styles.formContainer}>
                         <form className={styles.form}>
                             <label>
-                                <input placeholder='Food in mind?' type="text" name="Email" />
-                                <button>
+                                <input type='text' placeholder='Food in mind?' value={input} onChange={e => setInput(e.target.value)} />
+                                <button type='submit' onClick={handleSubmit}>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
                                         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
                                     </svg>
