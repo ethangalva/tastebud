17a0a3f0819aa43fe34aa9308388a2b5eb596123
ethangalva <82258388+ethangalva@users.noreply.github.com>
2022-11-29 06:17:48 -0800

    feat: finished static html code from app view

--- src/Pages/app/app.jsx
+++ src/Pages/app/app.jsx
@@ -15,17 +15,20 @@ function App(props) {
                         Delicious<br></br> food for you
                     </div>
                     <div className={styles.formContainer}>
-                                <form className={styles.form}>
-                                    <label>
-                                        <input placeholder='Search' type="text" name="Email" />
-                                        <button>
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                                            </svg>
-                                        </button>
-                                    </label>
-                                </form>
-                            </div>
+                        <form className={styles.form}>
+                            <label>
+                                <input placeholder='Food in mind?' type="text" name="Email" />
+                                <button>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                                    </svg>
+                                    <div>Search</div>
+                                </button>
+                            </label>
+                        </form>
+                    </div>
+                    {/* RECIPE LOADING AND PLACEHOLDER GO HERE */}
+                    
                 </div>
                 <Footer />
             </div>
